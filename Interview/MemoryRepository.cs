93eb88f9b01047c2e63018af5742ddc669d48368
Vikas Kumar <vikas7k@yahoo.com>
2017-10-31 13:32:58 +0000

    Initial commit

---
+++ Interview/MemoryRepository.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Interview
+{
+  public  class MemoryRepository<T> : IRepository<T> where T : IStoreable
+    {
+        private List<T> store;
+
+        public MemoryRepository()
+        {
+            store = new List<T>();
+        }
+
+        public IEnumerable<T> All()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(IComparable id)
+        {
+            
+        }
+
+        public void Save(T item)
+        {
+           
+        }
+
+        public T FindById(IComparable id)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static Predicate<T> StoreIdMatch(IComparable id)
+        {
+            return i => i.Id.Equals(id);
+        }
+    }
+}
